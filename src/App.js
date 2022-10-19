4ebd5ddbc55f866290b64a65a7aea2243d804b89
Josiah Brown <josiah.webdev@gmail.com>
2022-10-19 17:30:47 -0500

    Add basic routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/home/Home";
+import Add from "./routes/add/Add";
+import Edit from "./routes/edit/Edit";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add" element={<Add />} />
+        <Route path="/edit" element={<Edit />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
